f8ef8893546a727e12c586278a9b69a9a0edb0d5
Matej Hočevar <matej.hocevar@tretton37.com>
2024-02-04 09:25:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Self-driving car
+
+A self-driving car project written in Flutter/Dart.
---
+++ android/app/src/main/kotlin/si/matox/selfdrivingcar/self_driving_car/MainActivity.kt
@@ -0,0 +1,6 @@
+package si.matox.selfdrivingcar.self_driving_car
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
